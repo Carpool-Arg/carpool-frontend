b0edae49740db6e97fa83aafb77dc80165928d12
Roberto <robertosalera15@gmail.com>
2025-10-03 20:25:22 -0300

    [FEAT]: Visualizacion de los detalles de un viaje, nuevo skeleton para carga de componente y componente generico para errores.

--- src/types/response/trip.ts
+++ src/types/response/trip.ts
@@ -5,7 +5,9 @@ import { BaseResponse } from "./response";
 export interface TripDetailsData{
     id: number;
     driverName: string;
+    driverRating: number;
     startDateTime: string;
+    currentAvailableSeats:number;
     vehicle: VehicleResponseTripDTO;
     tripStops: TripStop[]
     availableSeat: number;
