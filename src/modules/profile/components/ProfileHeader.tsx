4072f4ccf674ec15cdc0133f9c810eb8fee3cf67
AnilAgustin <anil.agustin06@gmail.com>
2025-12-11 14:28:57 -0300

    [FIX] Incorporacion de div en foto de perfil para fijar el tamaño de la misma

--- src/modules/profile/components/ProfileHeader.tsx
+++ src/modules/profile/components/ProfileHeader.tsx
@@ -2,32 +2,28 @@ import { R2_PUBLIC_PREFIX } from '@/constants/imagesR2';
 import { useAuth } from '@/contexts/authContext';
 import Image from 'next/image';
 
-
 export function ProfileHeader() {
-  const { user, prevImage } = useAuth();
 
+  const { user, prevImage } = useAuth();
   const imageToShow = prevImage || user?.profileImage;
 
   return (
     <div className="flex flex-col items-center gap-2">
-      {imageToShow ? (
+
+      <div className="relative w-32 h-32 rounded-full overflow-hidden">
         <Image
-          src={imageToShow}
+          src={imageToShow || `${R2_PUBLIC_PREFIX}/default-profile.png`}
           alt="Foto de perfil"
-          width={96}
-          height={96}
-          className="rounded-full object-cover"
+          fill
+          sizes="128px"
+          className="object-cover"
         />
-      ) : (
-        <Image
-          src={`${R2_PUBLIC_PREFIX}/default-profile.png`}
-          alt="Foto de perfil"
-          width={96}
-          height={96}
-          className="rounded-full object-cover"
-        />
-      )}
-      <h2 className="text-xl font-semibold text-gray-2 dark:text-gray-1">{user?.username}</h2>
+      </div>
+
+      <h2 className="text-xl font-semibold text-gray-2 dark:text-gray-1">
+        {user?.username}
+      </h2>
     </div>
   );
 }
+
