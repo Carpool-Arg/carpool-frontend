505f786d18633cdd4c359821456bbf960b703619
Gonzalo Baldassi <gonzalobaldassi02@gmail.com>
2025-12-17 19:16:13 -0300

    Srg 135 fix/vertical scroll (#51)
    * [FIX]: Scroll vertical - dvh para que sea responsive en mobile
    
    * [FIX]: Cambiar el padding dentro del mobile para que no quede pegado el boton

--- src/layout/MobileLayout.tsx
+++ src/layout/MobileLayout.tsx
@@ -14,11 +14,11 @@ export default function MobileLayout({ children }: { children: React.ReactNode }
   const isLogoHeader = logoHeaderPaths.some(route => pathname.startsWith(route));
   
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col h-[100dvh] overflow-hidden">
       {showHeader && (
         <AppHeader showBack={!isLogoHeader} variant={isLogoHeader ? "logo" : "default"} />
       )}
-      <main className="flex-1 overflow-auto pt-8 pr-8 pl-8 pb-[80px]">
+      <main className="flex-1 overflow-y-auto px-8 pt-4 pb-[80px]">
         {children}
       </main>
       <MobileNavbar />
