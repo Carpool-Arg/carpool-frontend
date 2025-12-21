6a9d268dca361a13e66d21eb98c92cef27367430
Agustin <anil.agustin06@gmail.com>
2025-12-21 16:35:41 -0300

    [FIX] Validaciones realizadas en base a cambios del backend (#55)
    Co-authored-by: Roberto Salera <73187299+RoberSalera@users.noreply.github.com>

--- src/modules/profile/schemas/profileSchema.ts
+++ src/modules/profile/schemas/profileSchema.ts
@@ -1,17 +1,16 @@
 import { z } from "zod";
 
 export const profileSchema = z.object({
+  gender: z.enum(['MALE', 'FEMALE', 'UNSPECIFIED']).optional(),
+  phone: z.string()
+  .min(7, 'El teléfono debe tener al menos 7 dígitos')
+  .regex(/^[0-9+\-\s()]+$/, 'El número de teléfono debe contener únicamente números, guiones, signos + y espacios.')
+  .max(25, 'El teléfono no puede tener más de 25 dígitos'),
   name: z.string().optional(),
   lastname: z.string().optional(),
   dni: z.string().optional(),
   email: z.string().optional(),
   birthDate: z.string().optional(),
-  gender: z.enum(['MALE', 'FEMALE', 'UNSPECIFIED']),
-  phone: z
-  .string()
-  .regex(/^\d+$/, 'El número de teléfono solo puede contener números')
-  .min(8, 'Número demasiado corto')
-  .max(15, 'Número demasiado largo'),
 });
 
 export type ProfileData = z.infer<typeof profileSchema>;
