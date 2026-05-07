b6d895e8b266298130871e9b740de60a9e17d2d5
samuelbustosp <samuelbustospuntis@gmail.com>
2026-05-07 18:47:46 -0300

    [FIX] Error build

--- src/app/(app)/email-update/page.tsx
+++ src/app/(app)/email-update/page.tsx
@@ -1,12 +1,9 @@
 
 import EmailUpdatePage from "./EmailUpdate";
-import Spinner from "@/components/ux/Spinner";
+
 
 export default function EmailUpdate() {
     return(
-
         <EmailUpdatePage/>
-
-        
     ) 
 }
